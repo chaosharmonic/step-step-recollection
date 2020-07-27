de6836fb0f1af63a80476b89b84eb40cfffe7106
Sean <smorrissey@botkeeper.com>
2020-07-27 09:00:24 -0400

    Initial Session routing

--- src/controllers/session.js
+++ src/controllers/session.js
@@ -1,25 +1,74 @@
 import Session from '../models/session'
 
-export const getAllSessions = (req, res, next) => {
-  console.log('route: getAllSessions')
-}
-
-export const getSessionById = (req, res, next) => {
-  console.log('route: getSessionById')
-}
-
 export const getSessionsByPlayer = (req, res, next) => {
   console.log('route: getSessionsByPlayer')
 }
 
-export const addSession = (req, res, next) => {
-  console.log('route: addSession')
+export const getAllSessions = async (req, res, next) => {
+  try {
+    // TODO: pagination
+    // const { pageNo, pageSize } = req.query
+    // const query = { pageNo, pageSize }
+    const { filters } = req.body
+
+    const sessions = await Session.find({ ...filters })
+
+    res.json(sessions)
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-export const updateSession = (req, res, next) => {
-  console.log('route: updateSession')
+export const getSessionById = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    console.log(id)
+    const session = await Session.findOne({ _id: id })
+
+    const response = { ...session }
+    res.json(response)
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-export const deleteSession = (req, res, next) => {
-  console.log('route: deleteSession')
+export const addSession = async (req, res, next) => {
+  try {
+    const { payload } = req.body
+
+    const newSession = await Session.create({ ...payload })
+
+    res.json(newSession)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const updateSession = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const { payload } = req.body
+    let updatedSession = await Session.findOneAndUpdate({ _id: id }, { ...payload })
+
+    updatedSession = await Session.findOne({ _id: id })
+
+    res.json(updatedSession)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const deleteSession = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const isDeleted = await Session.deleteOne({ _id: id })
+
+    if (!isDeleted) throw new Error('Could not delete this id!')
+
+    res.json({
+      _id: id
+    })
+  } catch (err) {
+    console.error(err)
+  }
 }
--- src/routes/session.js
+++ src/routes/session.js
@@ -3,11 +3,11 @@ import * as sessionController from '../controllers/session.js'
 
 const sessionRouter = Router()
 
-sessionRouter.get('/', (req, res, next) => sessionController.getAllSessions())
-sessionRouter.get('/entry/:id', (req, res, next) => sessionController.getSessionById(id))
-sessionRouter.get('/player/:id', (req, res, next) => sessionController.getSessionsByPlayer(id))
-sessionRouter.post('/add', (req, res, next) => sessionController.addSession())
-sessionRouter.put('/update/:id', (req, res, next) => sessionController.updateSession())
-sessionRouter.delete('/delete/:id', (req, res, next) => sessionController.deleteSession())
+sessionRouter.get('/', (req, res, next) => sessionController.getAllSessions(req, res, next))
+sessionRouter.get('/entry/:id', (req, res, next) => sessionController.getSessionById(req, res, next))
+sessionRouter.get('/player/:id', (req, res, next) => sessionController.getSessionsByPlayer(req, res, next))
+sessionRouter.post('/add', (req, res, next) => sessionController.addSession(req, res, next))
+sessionRouter.put('/update/:id', (req, res, next) => sessionController.updateSession(req, res, next))
+sessionRouter.delete('/delete/:id', (req, res, next) => sessionController.deleteSession(req, res, next))
 
 export default sessionRouter
