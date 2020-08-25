5fbbd61eea462e11d806dd393d53ac1fe806fb30
Sean <smorrissey@botkeeper.com>
2020-08-25 00:43:44 -0400

    Update date field on release

--- src/models/release.js
+++ src/models/release.js
@@ -1,11 +1,11 @@
 import { model, Schema } from 'mongoose'
 
 const releaseSchema = new Schema({
-  title: String,
+  title: { type: String, required: true },
   scale: String, // DDR (classic), DDR X, ITG
-  numPanels: Number, // chart type -- DDR, Pump, StepmaniaX, etc.
-  releaseYear: Number,
-  releaseType: { type: String } // first or third party TODO: Create enums for this
+  numPanels: { type: Number, required: true }, // chart type -- DDR, Pump, StepmaniaX, etc.
+  releaseDate: Date,
+  releaseType: { type: String } // first or third party TODO: enums: ['arcade', 'console', 'custom']
 })
 
 const Release = model('Release', releaseSchema)
